8fa7062fe15483deaeba96377f44e8515d50011b
luispscarvalho <luispscarvalho@gmail.com>
2015-08-13 20:49:16 -0300

    commits becomes timeline

--- visminerweb/src/br/edu/ufba/softvis/visminerweb/view/Timeline.java
+++ visminerweb/src/br/edu/ufba/softvis/visminerweb/view/Timeline.java
@@ -4,8 +4,6 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
@@ -14,22 +12,18 @@ import javax.faces.bean.ViewScoped;
 import br.edu.ufba.softvis.visminer.model.business.Commit;
 import br.edu.ufba.softvis.visminer.model.business.File;
 import br.edu.ufba.softvis.visminer.model.business.SoftwareUnit;
-import br.edu.ufba.softvis.visminer.model.business.Tree;
-import br.edu.ufba.softvis.visminer.retriever.CommitRetriever;
-import br.edu.ufba.softvis.visminer.retriever.RetrieverFactory;
 import br.edu.ufba.softvis.visminer.retriever.SoftwareUnitRetriever;
 import br.edu.ufba.softvis.visminerweb.beans.AffectedFile;
 import br.edu.ufba.softvis.visminerweb.beans.Unit;
+import br.edu.ufba.softvis.visminerweb.factory.RetrieverFactory;
 
-@ManagedBean(name = "commits")
+@ManagedBean(name = "timeline")
 @ViewScoped
-public class Commits {
+public class Timeline {
 
 	@ManagedProperty(value = "#{selector}")
 	private Selector selector;
 
-	private int commitsCount = 0, commitId = -1;
-	private Map<Integer, Commit> commitsMap = new TreeMap<Integer, Commit>();
 	private List<AffectedFile> affectedFiles = new ArrayList<AffectedFile>();
 
 	private Date initialDate = null, finalDate = null;
@@ -44,28 +38,12 @@ public class Commits {
 		this.selector = selector;
 	}
 
-	private List<Commit> getCommits(Tree tree) {
-		CommitRetriever retriever = RetrieverFactory
-				.create(CommitRetriever.class);
-
-		commitsMap.clear();
-		List<Commit> commits = retriever.retrieveByTree(tree);
-		for (Commit commit : commits) {
-			commitsMap.put(commit.getId(), commit);
-		}
-		return commits;
-	}
-
-	public Map<Integer, Commit> getCommitsMap() {
-		return commitsMap;
-	}
-
 	public String getTimelineData() {
 		String data = "var items = new vis.DataSet({type: { start: 'ISODate', end: 'ISODate' }});";
 		data += "items.add([";
 
-		commitsCount = 1;
-		List<Commit> commits = getCommits(selector.getCurrentTree());
+		int count = 1;
+		List<Commit> commits = selector.getCommits();
 		for (Commit commit : commits) {
 			Date commitDate = commit.getDate();
 
@@ -81,16 +59,16 @@ public class Commits {
 			sdf = new SimpleDateFormat("hh:mm");
 			String time = sdf.format(commit.getDate());
 
-			if (commitsCount > 1) {
+			if (count > 1) {
 				data += ",";
 			}
 
-			data += "{id: " + commitsCount
+			data += "{id: " + count
 					+ ", content: '<a href=\"#\" onclick=\"setCommitId("
 					+ commit.getId() + "); return true;\">" + time
 					+ "</a>', start: '" + date + "'}";
 
-			commitsCount++;
+			count++;
 		}
 		data += "]);";
 
@@ -100,23 +78,11 @@ public class Commits {
 	public void resetFilter() {
 		initialDate = finalDate = null;
 	}
-	
-	public void setCommitId(int id) {
-		commitId = id;
-	}
-
-	public int getCommitId() {
-		return commitId;
-	}
-
-	public int getCommitsCount() {
-		return commitsCount;
-	}
 
 	public void submmitChoice() {
 		affectedFiles.clear();
 
-		Commit commit = getSelectedCommit();
+		Commit commit = selector.getSelectedCommit();
 		if (commit != null) {
 			for (File file : commit.getCommitedFiles()) {
 				affectedFiles.add(new AffectedFile(file));
@@ -124,25 +90,15 @@ public class Commits {
 		}
 	}
 
-	public Commit getSelectedCommit() {
-		Commit commit = null;
-
-		if (commitId != -1) {
-			commit = commitsMap.get(commitId);
-		}
-
-		return commit;
-	}
-
 	public List<Unit> getSoftwareUnits() {
 		List<Unit> units = new ArrayList<Unit>();
 
-		if ((commitId != -1) && (selectedFile != null)) {
+		if ((selector.getCommitId() != -1) && (selectedFile != null)) {
 			SoftwareUnitRetriever retriever = RetrieverFactory
 					.create(SoftwareUnitRetriever.class);
 
 			List<SoftwareUnit> softwareUnits = retriever.findByFile(
-					selectedFile.getId(), commitId);
+					selectedFile.getId(), selector.getCommitId());
 			for (SoftwareUnit swu : softwareUnits) {
 				units.add(new Unit(swu));
 
