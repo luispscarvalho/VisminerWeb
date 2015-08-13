34329624396fbcdc66b3508bcef081d0f2ab5707
luispscarvalho <luispscarvalho@gmail.com>
2015-08-13 20:49:37 -0300

    + functions delegated to selector

--- visminerweb/src/br/edu/ufba/softvis/visminerweb/view/Selector.java
+++ visminerweb/src/br/edu/ufba/softvis/visminerweb/view/Selector.java
@@ -2,6 +2,8 @@ package br.edu.ufba.softvis.visminerweb.view;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
@@ -10,10 +12,12 @@ import javax.faces.bean.ViewScoped;
 import org.primefaces.model.DefaultTreeNode;
 import org.primefaces.model.TreeNode;
 
+import br.edu.ufba.softvis.visminer.model.business.Commit;
 import br.edu.ufba.softvis.visminer.model.business.Repository;
 import br.edu.ufba.softvis.visminer.model.business.Tree;
+import br.edu.ufba.softvis.visminer.retriever.CommitRetriever;
 import br.edu.ufba.softvis.visminer.retriever.RepositoryRetriever;
-import br.edu.ufba.softvis.visminer.retriever.RetrieverFactory;
+import br.edu.ufba.softvis.visminerweb.factory.RetrieverFactory;
 
 @ManagedBean(name = "selector")
 @ViewScoped
@@ -21,24 +25,28 @@ public class Selector {
 
 	private Repository repository;
 	private List<Repository> repositories = new ArrayList<Repository>();
+	private Map<Integer, Commit> commitsMap = new TreeMap<Integer, Commit>();
+
+	private int commitId = -1;
 
 	private TreeNode treeNodes;
-	private Tree currentTree;
+	private Tree selectedTree;
 
 	@PostConstruct
 	private void initRepositories() {
-		RepositoryRetriever retriever = RetrieverFactory.create(RepositoryRetriever.class);
-		
+		RepositoryRetriever retriever = RetrieverFactory
+				.create(RepositoryRetriever.class);
+
 		repositories.clear();
 		repositories.addAll(retriever.retrieveAll());
 	}
-	
+
 	private void buildTree() {
 		if (repository != null) {
 			List<Tree> trees = repository.getTrees();
 
 			treeNodes = new DefaultTreeNode(repository.getName(), null);
-			
+
 			for (Tree t : trees) {
 				new DefaultTreeNode(t, treeNodes);
 			}
@@ -65,12 +73,51 @@ public class Selector {
 		return treeNodes;
 	}
 
-	public void setCurrentTree(Tree tree) {
-		this.currentTree = tree;
+	public void setSelectedTree(Tree tree) {
+		this.selectedTree = tree;
 	}
 
-	public Tree getCurrentTree() {
-		return currentTree;
+	public Tree getSelectedTree() {
+		return selectedTree;
+	}
+
+	public List<Commit> getCommits() {
+		List<Commit> commits = new ArrayList<Commit>();
+		commitsMap.clear();
+
+		if (selectedTree != null) {
+			CommitRetriever retriever = RetrieverFactory
+					.create(CommitRetriever.class);
+
+			commits.addAll(retriever.retrieveByTree(selectedTree));
+			for (Commit commit : commits) {
+				commitsMap.put(commit.getId(), commit);
+			}
+		}
+
+		return commits;
+	}
+
+	public Map<Integer, Commit> getCommitsMap() {
+		return commitsMap;
+	}
+
+	public void setCommitId(int id) {
+		commitId = id;
+	}
+
+	public int getCommitId() {
+		return commitId;
+	}
+
+	public Commit getSelectedCommit() {
+		Commit commit = null;
+
+		if (commitId != -1) {
+			commit = commitsMap.get(commitId);
+		}
+
+		return commit;
 	}
 
 }
